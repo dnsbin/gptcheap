9b449c20f2a07d64544c5d28d7b75567359cf250
Amar867 <amarafzal2008@gmail.com>
2026-01-16 16:34:17 -0500

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { createClient } = require("@supabase/supabase-js");
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
